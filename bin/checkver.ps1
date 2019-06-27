82f4c97dc7417005ddd49c7e431a125d7bb8452d
batkiz <batkiz@outlook.com>
2019-06-27 19:01:37 +0800

    update bin

--- bin/checkver.ps1
+++ bin/checkver.ps1
@@ -1,4 +1,12 @@
-if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = resolve-path (split-path (split-path (scoop which scoop))) }
-$checkver = "$env:SCOOP_HOME/bin/checkver.ps1"
-$dir = "$psscriptroot/.." # checks the parent dir
-iex -command "$checkver -dir $dir $($args |% { "$_ " })"
+#Requires -Version 5
+param(
+    [String] $dir = "$PSScriptRoot\..\bucket",
+    [Parameter(ValueFromRemainingArguments = $true)]
+    [String[]] $remainArgs = @()
+)
+
+if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
+$checkver = "$env:SCOOP_HOME\bin\checkver.ps1"
+$remainArgs = ($remainArgs | Select-Object -Unique) -join ' '
+
+Invoke-Expression -command "$checkver -dir $dir $remainArgs"
--- bin/test.ps1
+++ bin/test.ps1
@@ -1,2 +1,5 @@
-if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = resolve-path (split-path (split-path (scoop which scoop))) }
-Invoke-Pester "$psscriptroot/.."
+#Requires -Version 5
+#Requires -Modules @{ ModuleName = 'Pester'; ModuleVersion = '4.4.0' }
+
+if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
+Invoke-Pester "$psscriptroot\.."
